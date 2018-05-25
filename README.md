6750ee850b8539a99f631f28b847f6e14d4e95f4
Chris Radoux <cradoux@ebi.ac.uk>
2018-05-25 09:38:21 +0100

    added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Open Targets Tractability Pipeline
+
+## Installation
+
+`pip install something`
+
+## Getting Started
+
+
+## Modifying the Pipeline
---
+++ run_pipeline.py
@@ -0,0 +1,24 @@
+from pipeline.buckets import *
+import pandas as pd
+
+# Get a unique list of Gene IDs from OT
+ot_data = pd.read_csv('all_targets.csv', encoding='utf-8')
+ensembl_id_list = list(ot_data['ensembl_gene_id'].unique())
+
+# URL to local ChEMBL database
+database_url = 'oracle://cradoux:cradoux@ora-vm-065.ebi.ac.uk:1531/chempro'
+
+# Assign tractability buckets
+setup = Pipeline_setup(ensembl_id_list)
+
+#sm = Small_molecule_buckets(setup, database_url=database_url)
+#sm_out_buckets = sm.assign_buckets()
+
+sm_out_buckets = pd.read_csv('out_buckets2.csv')
+#print(sm_out_buckets.groupby('Bucket')['ensembl_gene_id'].count())
+
+ab = Antibody_buckets(setup,database_url=database_url,sm_output=sm_out_buckets)
+ab_out_buckets = ab.assign_buckets()
+#print(ab_out_buckets.groupby('Bucket')['ensembl_gene_id'].count())
+
+ab_out_buckets.to_csv('ab_out_buckets.csv')
