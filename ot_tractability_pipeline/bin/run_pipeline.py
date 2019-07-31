68ba2356493d30bb01ca93b99b6a2277da283393
Chris Radoux <cradoux@ebi.ac.uk>
2019-07-31 10:06:06 +0100

    removed debugging steps from run script

--- ot_tractability_pipeline/bin/run_pipeline.py
+++ ot_tractability_pipeline/bin/run_pipeline.py
@@ -5,21 +5,21 @@ import datetime
 
 def run(ensembl_id_list, database_url, out_file_name):
 
-    smab_buckets = pd.read_csv('tractability_buckets.tsv', sep='\t')
-    # # Assign tractability buckets
+    #smab_buckets = pd.read_csv('tractability_buckets.tsv', sep='\t')
+    # Assign tractability buckets
 
-    # # From Ensembl gene ID, get Uniprot, GO.CC, Symbol and PDB codes
+    # From Ensembl gene ID, get Uniprot, GO.CC, Symbol and PDB codes
     setup = Pipeline_setup(ensembl_id_list)
-    #
-    # # Get small molecule tractability info
-    # sm = Small_molecule_buckets(setup, database_url=database_url)
-    # sm_out_buckets = sm.assign_buckets()
-    # print(sm_out_buckets.groupby('Top_bucket')['ensembl_gene_id'].count())
-    #
-    # # Get antibody tractability info
-    # ab = Antibody_buckets(setup,database_url=database_url,sm_output=sm_out_buckets)
-    # smab_buckets = ab.assign_buckets()
-    # print(smab_buckets.groupby('Top_bucket_ab')['accession'].count())
+
+    # Get small molecule tractability info
+    sm = Small_molecule_buckets(setup, database_url=database_url)
+    sm_out_buckets = sm.assign_buckets()
+    print(sm_out_buckets.groupby('Top_bucket')['ensembl_gene_id'].count())
+
+    # Get antibody tractability info
+    ab = Antibody_buckets(setup,database_url=database_url,sm_output=sm_out_buckets)
+    smab_buckets = ab.assign_buckets()
+    print(smab_buckets.groupby('Top_bucket_ab')['accession'].count())
 
     # Get protac tractability info
     protac = Protac_buckets(setup, ab_output=smab_buckets)
